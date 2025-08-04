49a28f9b317fcefd4908ef1bf49943e112b8b580
Anatolii Zorin <Zorger27@GMail.com>
2025-08-05 02:54:53 +0300

    Small changes

--- src/pages/menu/Project1.jsx
+++ src/pages/menu/Project1.jsx
@@ -11,9 +11,19 @@ export const Project1 = () => {
       <Helmet>
         <title>{t('project1.name')}</title>
         <meta name="description" content={t('project1.disc')} />
+
+        {/* Open Graph meta tags */}
         <meta property="og:title" content={t('project1.name')} />
         <meta property="og:description" content={t('project1.disc')} />
-        <meta property="og:image" content="@/assets/ogimage/project1.jpg" />
+        <meta property="og:image" content="https://react-js-template.vercel.app/ogimage/project1.jpg" />
+        <meta property="og:url" content="https://react-js-template.vercel.app" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="https://react-js-template.vercel.app" />
+
+        {/* Twitter meta tags */}
+        <meta property="twitter:title" content={t('project1.name')} />
+        <meta property="twitter:description" content={t('project1.disc')} />
+        <meta property="twitter:image" content="https://react-js-template.vercel.app/ogimage/project1.jpg" />
         <meta name="twitter:card" content="summary_large_image" />
       </Helmet>
       <div className="container">
