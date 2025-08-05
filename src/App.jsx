829906e09a8c74bc726ac03f93dd296601750e54
Anatolii Zorin <Zorger27@GMail.com>
2025-08-06 01:18:12 +0300

    Small changes

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ const AppLayout = () => {
           <Route path="/project3" element={<Project3 />} />
           <Route path="/about" element={<About />} />
           <Route path="/404" element={<PageNotFound />} />
-          <Route path="*" element={<PageNotFound />} />
+          {/*<Route path="*" element={<PageNotFound />} />*/}
 
           {/* Обработка неизвестных маршрутов */}
           {/*<Route path="*" element={<Navigate to="/404" replace />} />*/}
