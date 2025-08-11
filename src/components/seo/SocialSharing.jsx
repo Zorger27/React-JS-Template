f9999de43220401fb9e5cbe26b3990e29311df13
Anatolii Zorin <Zorger27@GMail.com>
2025-08-12 01:16:31 +0300

    Small changes

--- src/components/seo/SocialSharing.jsx
+++ src/components/seo/SocialSharing.jsx
@@ -13,10 +13,10 @@ export default function SocialSharing() {
   };
 
   const shareOnFacebook = () => {
-    // const shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${getShareUrl()}`;
+    const shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${getShareUrl()}`;
 
-    const url = encodeURIComponent(window.location.href);
-    const shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${url}`;
+    // const url = encodeURIComponent(window.location.href);
+    // const shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${url}`;
 
     window.open(
       shareUrl,
